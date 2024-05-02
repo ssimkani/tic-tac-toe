75e19755be85c7848f5707c13f562807542df27a
Seeba184 <seenasimkani@gmail.com>
2024-05-02 07:34:52 -0400

    Initialize TicTacToe class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,1 +1,11 @@
 # frozen_string_literal: true
+
+class TicTacToe
+  attr_accessor :player1, :player2, :board
+
+  def initialize
+    @player1 = ''
+    @player2 = ''
+    @board = "1 | 2 | 3\n__|___|___\n4 | 5 | 6\n__|___|___\n7 | 8 | 9\n  |   |\n"
+  end
+end
