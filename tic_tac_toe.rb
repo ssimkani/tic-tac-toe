934dce587f617614d952a2bc925638c36ef9d58b
Seeba184 <seenasimkani@gmail.com>
2024-05-02 07:37:37 -0400

    Add chose_players method to input names of players

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -8,4 +8,13 @@ class TicTacToe
     @player2 = ''
     @board = "1 | 2 | 3\n__|___|___\n4 | 5 | 6\n__|___|___\n7 | 8 | 9\n  |   |\n"
   end
+
+  protected
+
+  def chosen_players
+    print "Name of Player 1:\t"
+    @player1 = gets.chomp
+    print "\nName of Player 2:\t"
+    @player2 = gets.chomp
+  end
 end
